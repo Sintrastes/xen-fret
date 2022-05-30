7de46eabf596fd0d1df3f288145baa1960a12402
Nathan Bedell <nbedell@tulane.edu>
2022-05-30 11:26:36 -0400

    Add nav bar.

---
+++ src/Utils.hs
@@ -0,0 +1,62 @@
+
+module Utils where
+
+import Reflex.Dom.Core
+import qualified Data.Text as T
+import Data.Functor
+import Control.Monad
+import Control.Monad.Fix
+
+-- | Nav bar widget.
+materialNavBar :: (DomBuilder t m, MonadHold t m, MonadFix m, Show e, PostBuild t m) => [e] -> m (Event t e)
+materialNavBar tabs = mdo
+
+    -- Nav bar menu
+    (navPane, navPaneEvents) <- elDynAttr' "div" sidebarAttrs $ el "ul" $ do
+        tabEvents        <- forM tabs (\tab -> do
+            events <- sidebarButton (T.pack $ show tab)
+            pure $ tab <$ events)
+
+        pure $ leftmost tabEvents
+
+    (navBarEvents, toggleMenuEvent) <- elAttr "div" ("style" =: "user-select: none;") $ elAttr "nav" ("class" =: "unselectable nav-wrapper") $ el "div" $ do
+        navMenu <- elAttr' "a" ("class" =: "unselectable-btn sidenav-trigger" <> "unselectable" =: "on") $
+            elClass "i" "material-icons" $ text "menu"
+        
+        elAttr "ul" ("id" =: "nav-mobile" <> "class" =: "left hide-on-med-and-down") $ do
+            menuEvents <- forM tabs (\tab -> do
+                btnEvents <- navButton (T.pack $ show tab)
+                pure $ tab <$ btnEvents)
+
+            pure (leftmost menuEvents, domEvent Click (fst navMenu))
+
+    let outsideNavBarEvents = difference (domEvent Click overlay)
+            (domEvent Click navPane)
+
+    sidebarOpened <- accumDyn (\s _ -> not s) False
+        (leftmost [() <$ toggleMenuEvent, () <$ navPaneEvents, () <$ outsideNavBarEvents])
+
+    (overlay, _) <- elDynAttr' "div" overlayAttrs blank
+
+    let sidebarAttrs = sidebarOpened <&> \isOpened ->
+          "class" =: "w3-sidebar w3-bar-block w3-border-right" <>
+            if isOpened
+                then "style" =: "display: block; z-index: 999;"
+                else "style" =: "display: none;"
+    
+    let overlayAttrs = sidebarOpened <&> \isOpened ->
+          "class" =: "sidenav-overlay" <>
+            if isOpened 
+                then "style" =: "display: block; opacity: 1;"
+                else "style" =: "display: none;"
+
+    pure $ leftmost [navBarEvents, navPaneEvents]
+
+sidebarButton x = el "li" $
+    domEvent Click . fst <$>
+        elClass' "a" "unselectable w3-bar-item w3-button"
+            (text x)
+
+navButton x = el "li" $
+    domEvent Click . fst <$>
+        el' "a" (text x)
