48f4cb3a8bf7d8a71e7f744ce619a727db44cac4
Nathan Bedell <nbedell@tulane.edu>
2022-05-29 21:53:45 -0400

    Update gitignore.

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,15 @@
 module Main where
 
 import Network.CGI
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, isNothing)
 import Control.Monad
-import Data.Either.Utils
 import Data.Either
-import Diagrams.Prelude
+import Diagrams.Prelude hiding (output)
 import Diagrams.Backend.SVG
-import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Frets.Util
 import Frets
+import Graphics.Svg
 
 -- Constants for now, TODO: allow manual control from the CGI interface.
 vs = 0.2
@@ -31,72 +30,65 @@ cgiMain = do
     t <- readInput "tuning" :: CGI (Maybe [Int])
     x <- readInput "x" :: CGI (Maybe Int)
     y <- readInput "y" :: CGI (Maybe Int)
-    
+
     -- Handle errors parsing the cgi arguments
-    case (handleCGIParseErrs p f s t x y) of 
+    case handleCGIParseErrs p f s t x y of
         Left err                              -> output err
         Right (period,frets,scales,tuning,xy) -> do
             let _fretboard = mkFret tuning period
             let _scales    = map (mkScl period) scales
-            case (handleScaleFretboardErrs _fretboard _scales) of
-                Left err                 -> output err
+            case handleScaleFretboardErrs _fretboard _scales of
+                Left err                 -> output $ concatErrors err
                 Right (fretboard,scales) -> do
-                    let diagrams = map (\scale -> toBoard frets vs hs (chScale fretboard scale)) scales
+                    let diagrams = map (toBoard frets vs hs . chScale fretboard) scales
                     case xy of
                         X x -> output $ foldl1 (\x y -> x++"\n &nbsp;&nbsp;&nbsp; \n"++y) $ map (format (X x)) diagrams
                         Y y -> output $ foldl1 (\x y -> x++"\n &nbsp;&nbsp;&nbsp; \n"++y) $ map (format (Y y)) diagrams
 
 -- | Generate the formatted SVG output from a diagram.
-format :: XorY -> Diagram B R2 -> String
-format (X x) d = B.unpack $ renderSvg $ renderDia SVG (SVGOptions (Width (fromIntegral x)) Nothing) d
-format (Y y) d = B.unpack $ renderSvg $ renderDia SVG (SVGOptions (Height (fromIntegral y)) Nothing) d
+format :: XorY -> Diagram B -> String
+format (X x) d = B.unpack $ renderBS $ renderDia SVG (SVGOptions (mkWidth (fromIntegral x)) Nothing "" [] False) d
+format (Y y) d = B.unpack $ renderBS $ renderDia SVG (SVGOptions (mkWidth (fromIntegral y)) Nothing "" [] False) d
 
 -- | Handle the error messages from parsing the arguments.
 handleCGIParseErrs :: Maybe Int -> Maybe Int -> Maybe [[Int]]
        -> Maybe [Int] -> Maybe Int -> Maybe Int
-       -> (Either String (Int,Int,[[Int]],[Int],XorY))
-handleCGIParseErrs p f s t x y 
+       -> Either String (Int,Int,[[Int]],[Int],XorY)
+handleCGIParseErrs p f s t x y
   -- Valid format
   | Just p' <- p
   , Just f' <- f
   , Just s' <- s
   , Just t' <- t
   , isJust x `xor` isJust y
-  = case (x,y) of 
+  = case (x,y) of
        (Just x',_) -> Right (p',f',s',t',X x')
        (_,Just y') -> Right (p',f',s',t',Y y')
+       _           -> Left "Must have either x or y specified."
   -- Errors, invalid format
   |  otherwise
-  = collectErrors [(p == Nothing, "Error parsing period, should be an integer"),
-                   (f == Nothing, "Error parsing frets, should be an integer"),
-                   (s == Nothing, "Error parsing scales, should be a list of a list of integers (e.a. [[1,2,3],[4,5,6]])"),
-                   (t == Nothing, "Error parsing tuning, should be a list of integers, (e.a. [1,2,3])"),
-                   ((x == Nothing) && (y == Nothing), "Neither given an x, nor a y"),
-                   ((isJust x) && (isJust y), "Given both an x and a y, only one or the other can be given")]
+  = collectErrors [(isNothing p, "Error parsing period, should be an integer"),
+                   (isNothing f, "Error parsing frets, should be an integer"),
+                   (isNothing s, "Error parsing scales, should be a list of a list of integers (e.a. [[1,2,3],[4,5,6]])"),
+                   (isNothing t, "Error parsing tuning, should be a list of integers, (e.a. [1,2,3])"),
+                   (isNothing x && isNothing y, "Neither given an x, nor a y"),
+                   (isJust x && isJust y, "Given both an x and a y, only one or the other can be given")]
 
 -- | Handle the error messages from constructing the scales and fretboard
-handleScaleFretboardErrs :: Either [String] Fretboard -> [Either [String] Scale] -> Either String (Fretboard,[Scale])
-handleScaleFretboardErrs _fb _scls | isRight _fb
-                                   , all (==True) (map isRight _scls)
-                                   = Right (fromRight _fb,map fromRight _scls)
-                                   -- Remove redundant error messages when period is invalid
-                                   | isLeft _fb
-                                   , "Period must be a positive number" `elem` (fromLeft _fb)
-                                   = concatErrors $ (fromLeft _fb)
-                                   | isLeft _fb
-                                   = concatErrors $ (fromLeft _fb) ++ (indexErrs _scls)
-                                   | otherwise = concatErrors (indexErrs _scls)
+handleScaleFretboardErrs :: Either [String] Fretboard -> [Either [String] Scale] -> Either [String] (Fretboard,[Scale])
+handleScaleFretboardErrs fb scls = do
+    fb' <- fb
+    scls' <- sequence scls
+    pure (fb', scls')
 
 -- | Include which no. scale the errors are coming from.
 indexErrs :: [Either [String] a] -> [String]
 indexErrs xs = go 1 xs []
     where go n [] idx = idx
-          go n ((Left errs):xs) idx = go (n+1) xs (idx++["For scale #"++(show n)++": "++(fmt errs)])
+          go n ((Left errs):xs) idx = go (n+1) xs (idx++["For scale #" ++ show n ++ ": " ++ fmt errs])
           go n ((Right _):xs)   idx = go (n+1) xs idx
-          
-          fmt errs = (foldl1 (\x y -> x++", "++y) errs)
 
-
+          fmt errs = foldl1 (\x y -> x++", "++y) errs
 
 main :: IO ()
 main = runCGI (handleErrors cgiMain)
