fed6ec3a5f1a8c66b17e419f733a64289c360df6
Nathan Bedell <nbedell@tulane.edu>
2022-05-31 22:34:33 -0400

    Add more examples of scales.

--- src/Utils.hs
+++ src/Utils.hs
@@ -94,25 +94,45 @@ defaultAppData = AppData {
           , ("12-TET", Scale "Minor" 
                 (2 :| [1,2,2,1,2,2]))
           , ("12-TET", Scale "Dorian" 
-                (2 :| [3,5,7,9,10]))
+                (2 :| [1,2,2,2,1,2]))
           , ("13-TET", Scale "Archeotonic (Ryonian Mode)"
                 (2 :| [2,2,2,2,2,1]))
           , ("13-TET", Scale "Oneirotonic (Dylathian Mode)"
                 (2 :| [2,1,2,2,1,2,1]))
           , ("16-TET", Scale "mavila[7]"
-                (2 :| [2,2,2,2,2,1]))
+                (2 :| [2,2,3,2,2,3]))
           , ("16-TET", Scale "Lemba"
-                (2 :| [2,2,2,2,2,1]))
+                (3 :| [3,2,3,3,2]))
           , ("16-TET", Scale "Magic"
-                (2 :| [2,2,2,2,2,1]))
+                (1 :| [4,1,4,1,4,1]))
           , ("19-TET", Scale "Ionian (Major)"
-                (2 :| [2,1,2,2,2,1]))
-          , ("22-TET", Scale "Ionian (Major)"
-                (2 :| [2,1,2,2,2,1]))
+                (3 :| [3,2,3,3,3,2]))
+          , ("19-TET", Scale "Sensi[8]"
+                (2 :| [3,2,2,3,2,2,3]))
+          , ("19-TET", Scale "Negri[9]"
+                (2 :| [2,2,2,3,2,2,2,2]))
+          , ("19-TET", Scale "Kleismic[7]"
+                (1 :| [4,1,4,1,4,4]))
+          , ("22-TET", Scale "Superpyth[7] (Major)"
+                (4 :| [4,1,4,4,4,1]))
+          , ("22-TET", Scale "Astrology[6]"
+                (4 :| [3,4,4,3,4]))
+          , ("22-TET", Scale "Porcupine[7]"
+                (3 :| [3,3,4,3,3,3]))
+          , ("22-TET", Scale "Pajara[10]"
+                (2 :| [2,3,2,2,2,2,3,2,2]))
           , ("24-TET", Scale "Ionian (Major)"
-                (2 :| [2,1,2,2,2,1]))
+                (4 :| [4,2,4,4,4,2]))
+          , ("24-TET", Scale "Anchihoye: Ethiopia"
+                (2 :| [8, 3, 6, 5]))
+          , ("24-TET", Scale "Enharmonic Phrygian"
+                (8 :| [1, 1, 8, 4, 1, 1]))
+          , ("24-TET", Scale "Maqam Rast"
+                (4 :| [3, 3, 4, 4, 3, 3]))
+          , ("24-TET", Scale "Mohajira[7]"
+                (3 :| [4,3,4,3,4,3]))
           , ("Bohlen Pierce", Scale "Lambda"
-                (2 :| [2,1,2,2,2,1]))
+                (2 :| [1,1,2,1,2,1,2,1]))
         ]
     , preferences = defaultPreferences
 }
