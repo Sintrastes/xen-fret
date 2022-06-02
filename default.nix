63369049bde4adc6f982976dfdd4f774660e5278
Nathan Bedell <nbedell@tulane.edu>
2022-06-01 20:02:18 -0400

    Use obelisk instead of reflex platform.

--- default.nix
+++ default.nix
@@ -1,32 +1,22 @@
 
-   
-{ reflex-platform ? import ./nix/reflex-platform.nix {}
-, withHoogle ? false
-, useWarp ? true
+{ system ? builtins.currentSystem
+, obelisk ? import ./.obelisk/impl {
+    inherit system;
+  }
 }:
-reflex-platform.project ({ pkgs, ... }: {
-
-  inherit withHoogle;
-  inherit useWarp;
+with obelisk;
+project ./. ({ pkgs, ... }: {
 
   packages = {
     frontend = ./frontend;
     xen-fret = ./xen-fret;
   };
 
-  shells = {
-    ghc = ["frontend"];
-    ghcjs = ["frontend"];
-  };
-
-  shellToolOverrides = self: super: {
-  };
-
   overrides = self: super: {
-      doctest = pkgs.callHackageDirect {
+      doctest = self.callHackageDirect {
         pkg = "doctest";
         ver = "0.16.3";
         sha256 = "qU50YWyeM1QI3lGQwboJ0iUlC4c4YTOrv3u/aVagRlg=";
       } {};
-  };
+    };
 })
