eec4b89b0e4ecb03fe4ed6e4a7775110664b9244
Nathan Bedell <nbedell@tulane.edu>
2022-06-04 19:55:48 -0400

    Add download function.

---
+++ xen-fret/static/main.js
@@ -0,0 +1,6 @@
+function download(text, name, type) {
+    var a = document.getElementById("a");
+    var file = new Blob([text], {type: type});
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+  }
