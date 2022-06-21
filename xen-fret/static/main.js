db09a2f87b15a0c21e149f32003b3ee4532b1bfb
Nathan Bedell <nbedell@tulane.edu>
2022-06-21 17:50:09 -0400

    Add a function for getting a cookie.

--- xen-fret/static/main.js
+++ xen-fret/static/main.js
@@ -15,3 +15,19 @@ function setCookie(cname, cvalue, exdays) {
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
