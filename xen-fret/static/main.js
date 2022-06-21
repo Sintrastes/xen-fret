7a5928efdb8f6c72d30e76bc3d6e42f5b10c8169
Nathan Bedell <nbedell@tulane.edu>
2022-06-21 17:06:22 -0400

    Add function for setting cookie.

--- xen-fret/static/main.js
+++ xen-fret/static/main.js
@@ -7,4 +7,11 @@ function download(text, name, type) {
     setTimeout(function() {
         document.body.removeChild(a);
     }, 0);
-  }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
