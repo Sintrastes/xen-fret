f33a932bbb91ea6b1c20520231ba9b31ae1c0562
Nathan Bedell <nbedell@tulane.edu>
2022-07-01 22:04:19 -0400

    Stub out some functions for buildForm.

--- xen-fret/src/Reflex/Dom/Forms.hs
+++ xen-fret/src/Reflex/Dom/Forms.hs
@@ -4,6 +4,8 @@ module Reflex.Dom.Forms where
 import Data.Profunctor
 import Data.Functor.Compose
 import Reflex.Dom
+import Control.Lens
+import Control.Monad.State
 
 type Form t m a b
     = Star (Compose m (Dynamic t)) a b
@@ -18,3 +20,10 @@ initForm (Star f) x = getCompose $ f x
 
 (=.) :: Profunctor f => (x -> y) -> f y a -> f x a
 (=.) = lmap
+
+data SomeLens a = forall b. SomeLens (Lens' a b)
+
+type FormBuilder t m a = StateT [SomeLens a] m a
+
+buildForm :: FormBuilder t m a -> SForm t m a
+buildForm f = undefined
