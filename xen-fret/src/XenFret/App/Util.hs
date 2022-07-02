6b3ea9dd16a83a6b539a28b422f1cc74546a5e31
Nathan Bedell <nbedell@tulane.edu>
2022-07-02 14:53:14 -0400

    Add an intervalListEntry widget.

--- xen-fret/src/XenFret/App/Util.hs
+++ xen-fret/src/XenFret/App/Util.hs
@@ -1,3 +1,35 @@
 
 module XenFret.App.Util where
 
+import qualified Data.Text as T
+import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as NE
+import Data.Validation
+import Reflex.Dom.Core
+import Reflex.Dom.Extras
+import Text.Read (readMaybe)
+import Data.Functor
+import Data.Function
+
+intervalListEntry :: _ => NonEmpty Int -> m (Dynamic t (Validation (NonEmpty T.Text) (NonEmpty Int)))
+intervalListEntry = validatedTextEntry
+    parseIntervalList
+    showIntervalList
+
+parseIntervalList :: T.Text -> Validation (NonEmpty T.Text) (NonEmpty Int)
+parseIntervalList text = fromEither $ do
+    let intervals = T.splitOn " " text
+
+    parsed <- intervals 
+        <&> T.unpack
+        <&> readMaybe @Int
+        <&> maybe (Left $ "Intervals must be whole numbers" :| []) 
+                Right
+          & sequence
+    
+    maybe (Left $ "List of intervals cannot be empty" :| []) Right $ 
+        NE.nonEmpty parsed
+
+showIntervalList :: NonEmpty Int -> T.Text
+showIntervalList xs = T.intercalate " " $ map (T.pack . show) $ NE.toList xs 
+
