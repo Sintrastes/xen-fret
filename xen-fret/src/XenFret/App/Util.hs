0e3d1961e58bc72a850d2c6ba1301c96d9329767
Nathan Bedell <nbedell@tulane.edu>
2022-07-03 13:37:49 -0400

    Add stub for composing validations.

--- xen-fret/src/XenFret/App/Util.hs
+++ xen-fret/src/XenFret/App/Util.hs
@@ -17,10 +17,13 @@ intervalListEntry = validatedTextEntry
     (fromEither <$> parseIntervalList)
     showIntervalList
 
-scaleListEntry :: _ => Int -> NonEmpty Int -> m (Dynamic t (Validation (NonEmpty T.Text) (NonEmpty Int)))
-scaleListEntry period = validatedTextEntry
-    (fromEither <$> (validateSumToPeriod period <=< parseIntervalList))
+scaleListEntry :: _ => Dynamic t Int -> NonEmpty Int -> m (Dynamic t (Validation (NonEmpty T.Text) (NonEmpty Int)))
+scaleListEntry period = validatedTextEntryDyn
+    ((fromEither <$>) <$> (validateSumToPeriod period `composeValidations` pure parseIntervalList))
     showIntervalList
+
+composeValidations :: Dynamic t (b -> Either e c) ->  Dynamic t (a -> Either e b) -> Dynamic t (a -> Either e c)
+composeValidations = undefined
 
 parseIntervalList :: T.Text -> Either (NonEmpty T.Text) (NonEmpty Int)
 parseIntervalList text = do
@@ -36,11 +39,11 @@ parseIntervalList text = do
     maybe (Left $ "List of intervals cannot be empty" :| []) Right $ 
         NE.nonEmpty parsed
 
-validateSumToPeriod :: Int -> NonEmpty Int -> Either (NonEmpty T.Text) (NonEmpty Int)
-validateSumToPeriod period intervals =
+validateSumToPeriod :: Reflex t => Dynamic t Int -> Dynamic t (NonEmpty Int -> Either (NonEmpty T.Text) (NonEmpty Int))
+validateSumToPeriod period' = period' <&> (\period intervals ->
     if sum intervals == period
         then return intervals
-        else Left $ ("Intervals did not sum to period (" <> T.pack (show period) <> ")") :| []
+        else Left $ ("Intervals did not sum to period (" <> T.pack (show period) <> ")") :| [])
 
 showIntervalList :: NonEmpty Int -> T.Text
 showIntervalList xs = T.intercalate " " $ map (T.pack . show) $ NE.toList xs 
