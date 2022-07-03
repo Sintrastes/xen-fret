737f38c29f03f6ad10178af46155fcc0c3d7e305
Nathan Bedell <nbedell@tulane.edu>
2022-07-03 13:28:42 -0400

    Add a scaleListEntry form that does validation of period.

--- xen-fret/src/XenFret/App/Util.hs
+++ xen-fret/src/XenFret/App/Util.hs
@@ -10,10 +10,16 @@ import Reflex.Dom.Extras
 import Text.Read (readMaybe)
 import Data.Functor
 import Data.Function
+import Control.Monad
 
 intervalListEntry :: _ => NonEmpty Int -> m (Dynamic t (Validation (NonEmpty T.Text) (NonEmpty Int)))
 intervalListEntry = validatedTextEntry
     (fromEither <$> parseIntervalList)
+    showIntervalList
+
+scaleListEntry :: _ => Int -> NonEmpty Int -> m (Dynamic t (Validation (NonEmpty T.Text) (NonEmpty Int)))
+scaleListEntry period = validatedTextEntry
+    (fromEither <$> (validateSumToPeriod period <=< parseIntervalList))
     showIntervalList
 
 parseIntervalList :: T.Text -> Either (NonEmpty T.Text) (NonEmpty Int)
@@ -29,6 +35,12 @@ parseIntervalList text = do
     
     maybe (Left $ "List of intervals cannot be empty" :| []) Right $ 
         NE.nonEmpty parsed
+
+validateSumToPeriod :: Int -> NonEmpty Int -> Either (NonEmpty T.Text) (NonEmpty Int)
+validateSumToPeriod period intervals =
+    if sum intervals == period
+        then return intervals
+        else Left $ ("Intervals did not sum to period (" <> T.pack (show period) <> ")") :| []
 
 showIntervalList :: NonEmpty Int -> T.Text
 showIntervalList xs = T.intercalate " " $ map (T.pack . show) $ NE.toList xs 
