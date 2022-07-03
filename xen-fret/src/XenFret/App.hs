387cad5d4ed9abcdbd44b36a0c1c031b1f5f0c6a
Nathan Bedell <nbedell@tulane.edu>
2022-07-03 13:13:40 -0400

    Fix issue with scale validation updates.

--- xen-fret/src/XenFret/App.hs
+++ xen-fret/src/XenFret/App.hs
@@ -481,26 +481,28 @@ scaleForm appData isValidName initialValue = mdo
           [ pure $ nonEmptyText "Scale name must not be entry"
           , isValidName <*> currentTemperament]
 
-    let temperamentForm = fmap (validateNonNull "Temperament must be selected" <$>)
-          . selectTemperament appData
+    temperamentForm <- (validateNonNull "Temperament must be selected" <$>) <$> 
+            selectTemperament appData initialTemperament
 
-    let temperamentUpdated = fst <$>
-          fmapMaybe validationToMaybe (updated res)
+    let temperamentUpdated = fmapMaybe validationToMaybe 
+            (updated temperamentForm)
 
-    let scaleForm = Scale <$>
+    scaleForm <- initFormA (Scale <$>
           formA (scaleName =. labeledEntryA "Name"
             (validatedTextEntryDyn scaleValidation id)) <*>
           formA (scaleIntervals =. labeledEntryA "Intervals"
-            intervalListEntry)
+            intervalListEntry)) initialValue
 
-    let formContents = (,) <$>
-          formA (fst =. temperamentForm) <*>
-          (snd =. scaleForm)
+    pure $ pairForms temperamentForm scaleForm
 
-    res <- initFormA formContents
-        (initialTemperament, initialValue)
-
-    pure res
+pairForms :: (Semigroup e, Reflex t) => 
+    Dynamic t (Validation e a) 
+ -> Dynamic t (Validation e b)
+ -> Dynamic t (Validation e (a, b))
+pairForms x y = do
+    x' <- x
+    y' <- y
+    pure $ (,) <$> x' <*> y'
 
 validationToMaybe = \case
     Success x -> Just x
@@ -675,6 +677,7 @@ githubWidget = do
     starsIcon :: _ => m ()
     starsIcon = elSvg "svg" ("style" =: "margin-left: 0.4rem; height: 0.6rem; width: 0.6rem;" <>"viewBox" =: "0 0 16 16" <> "xmlns" =: "http://www.w3.org/2000/svg") $
         elSvg "path" ("fill-rule" =: "evenodd" <> "d" =: "M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694v.001z") blank
+
 
 
 
