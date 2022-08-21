360c42924fba73fc640e2e5d1daebedd94ce0908
Nathan Bedell <nbedell@tulane.edu>
2022-08-21 16:36:36 -0600

    Finish implementing editing for scales.

--- xen-fret/src/XenFret/App.hs
+++ xen-fret/src/XenFret/App.hs
@@ -39,6 +39,8 @@ import Data.Validation
 import XenFret.App.Util
 import Control.Monad.Fix
 import Data.Map (Map, lookup, insert)
+import qualified Data.Sequence as Seq
+import Data.Foldable
 
 baseVerticalSpacing :: Double
 baseVerticalSpacing = 0.2
@@ -655,7 +657,7 @@ scalePage appDir = mdo
             currentScales <- getScales appDir
             pure (addScale temperament scale currentScales))
 
-    let updatedScales = leftmost [addedScale, removedScale]
+    let updatedScales = leftmost [addedScale, removedScale, editedScale]
 
     dynScales <- holdDyn currentScales
         updatedScales
@@ -684,7 +686,7 @@ scalePage appDir = mdo
                     elClass "li" "collection-item" $ do
                         deleteEvent <- domEvent Click . fst <$> elClass' "i" "material-icons" (
                             text "clear")
-                        editEvent <- domEvent Click . fst <$> elClass' "i" "material-icons" 
+                        editEvent <- domEvent Click . fst <$> elClass' "i" "material-icons"
                             (text "edit")
                         el "span" $ text $
                             T.pack $ show scale
@@ -694,15 +696,33 @@ scalePage appDir = mdo
     let removedScale = pushAlways (\(temperamentName, deletedScale) -> do
             currentScales <- getScales appDir
             let scales = currentScales Map.! temperamentName
-            let updatedScales = Map.insert temperamentName 
+            let updatedScales = Map.insert temperamentName
                     (filter (/= deletedScale) scales) currentScales
 
             pure updatedScales) deleteEvents
 
-    completeEditDialog <- validatedModal editEvents $ \(temperament, scale) ->
-        scaleForm appData 
-            (fromJust $ find (\x -> temperamentName x == temperament) $ temperaments appData) 
+    completeEditDialog <- validatedModal editEvents $ \(temperament, scale) -> do
+        res <- scaleForm appData
+            (fromJust $ find (\x -> temperamentName x == temperament) $ temperaments appData)
             (isNewName (Just $ scaleName scale)) scale
+        pure $ fmap (\(x,y) -> (x, scale, y)) <$> res
+
+    let editSubmitted = mapMaybe id completeEditDialog
+
+    let editedScale = pushAlways (\(temperament, origScale, newScale) -> do
+            currentScales <- getScales appDir
+            let scales = currentScales Map.! temperamentName temperament
+            let indexOfScale = fromJust $ scales & elemIndex origScale
+            let updatedScales = Seq.fromList scales
+                    & Seq.update indexOfScale newScale
+                    & toList
+
+            let updatedMap = Map.insert 
+                    (temperamentName temperament)
+                    updatedScales
+                    currentScales
+
+            pure updatedMap) editSubmitted
 
     blank
 
@@ -826,6 +846,7 @@ githubWidget = do
     starsIcon :: _ => m ()
     starsIcon = elSvg "svg" ("style" =: "margin-left: 0.4rem; height: 0.6rem; width: 0.6rem;" <>"viewBox" =: "0 0 16 16" <> "xmlns" =: "http://www.w3.org/2000/svg") $
         elSvg "path" ("fill-rule" =: "evenodd" <> "d" =: "M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.75.75 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694v.001z") blank
+
 
 
 
