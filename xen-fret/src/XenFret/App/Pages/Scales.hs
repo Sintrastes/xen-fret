26fbcd28b2640249dc9bc721b8a14c7214124ca7
Nathan Bedell <nbedell@tulane.edu>
2022-08-27 19:29:14 -0600

    Finish seperating out pages into different modules.

--- xen-fret/src/XenFret/App/Pages/Scales.hs
+++ xen-fret/src/XenFret/App/Pages/Scales.hs
@@ -1,2 +1,143 @@
 
 module XenFret.App.Pages.Scales where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import Data.List.NonEmpty (NonEmpty(..))
+import Data.Validation
+import qualified Data.List.NonEmpty as NE
+import XenFret.App.Widgets.Fretboard
+import XenFret.Data
+import XenFret.AppData
+import XenFret.Util
+import XenFret.App.Util
+import XenFret.AppData
+import Reflex.Dom.Core hiding(Home, button, checkbox)
+import Reflex.Dom.Extras
+import Reflex.Dom.Forms
+import qualified Data.Sequence as Seq
+import Data.List
+import Data.Maybe hiding (mapMaybe)
+import Data.Functor
+import Control.Monad
+import Data.Foldable
+import XenFret.App.Widgets.Fretboard
+
+scalePage :: _ => FilePath -> m ()
+scalePage appDir = mdo
+    appData <- loadAppData (appDir <> "/app_data.json")
+    let currentScales = scales appData
+
+    newScaleClick <- button "New Scale"
+
+    addedScale <- switch . current <$> prerender (pure never) (performEvent $ newScaleSubmitted <&> \case
+        Nothing -> pure currentScales
+        Just (temperament, scale) -> do
+            toast "Added new temperament"
+            currentScales <- getScales appDir
+            pure (addScale temperament scale currentScales))
+
+    let updatedScales = leftmost [addedScale, removedScale, editedScale]
+
+    dynScales <- holdDyn currentScales
+        updatedScales
+
+    -- Dynamic check for whether or not a scale's name already exists in the list of scales for a given temperament.
+    let isNewName initialName = dynScales <&> \scales temperament name -> 
+          let names = fmap scaleName (fromMaybe [] $ Map.lookup (temperamentName temperament) scales) in
+            if name `elem` names && (Just name /= initialName)
+                then Failure $ "There is already a scale with this name." :| []
+                    else Success name
+
+    newScaleSubmitted <- validatedModal newScaleClick $ \_ ->
+        scaleForm appData (head $ temperaments appData) (isNewName Nothing) def
+
+    let dynAppData = dynScales <&> \s ->
+            appData { scales = s }
+
+    persistAppData dynAppData
+        (appDir <> "/app_data.json")
+
+    (editEvents, deleteEvents) <- fanEither <$> (switchHold never =<< dyn (dynScales <&> \currentScales ->
+        elClass "ul" "collection" $ do
+            itemEvents <- forM (Map.toList currentScales) $ \(temperamentName, scales) -> do
+                el "h3" $ text temperamentName
+                forM scales $ \scale -> do
+                    elClass "li" "collection-item" $ do
+                        deleteEvent <- domEvent Click . fst <$> elClass' "i" "material-icons" (
+                            text "clear")
+                        editEvent <- domEvent Click . fst <$> elClass' "i" "material-icons"
+                            (text "edit")
+                        el "span" $ text $
+                            T.pack $ show scale
+                        pure $ leftmost 
+                            [
+                              deleteEvent $> Right (temperamentName, scale)
+                            , editEvent $> Left (temperamentName, scale)
+                            ]
+            pure (leftmost $ join itemEvents)))
+
+    let removedScale = pushAlways (\(temperamentName, deletedScale) -> do
+            currentScales <- getScales appDir
+            let scales = currentScales Map.! temperamentName
+            let updatedScales = Map.insert temperamentName
+                    (filter (/= deletedScale) scales) currentScales
+
+            pure updatedScales) deleteEvents
+
+    completeEditDialog <- validatedModal editEvents $ \(temperament, scale) -> do
+        res <- scaleForm appData
+            (fromJust $ find (\x -> temperamentName x == temperament) $ temperaments appData)
+            (isNewName (Just $ scaleName scale)) scale
+        pure $ fmap (\(x,y) -> (x, scale, y)) <$> res
+
+    let editSubmitted = mapMaybe id completeEditDialog
+
+    let editedScale = pushAlways (\(temperament, origScale, newScale) -> do
+            currentScales <- getScales appDir
+            let scales = currentScales Map.! temperamentName temperament
+            let indexOfScale = fromJust $ scales & elemIndex origScale
+            let updatedScales = Seq.fromList scales
+                    & Seq.update indexOfScale newScale
+                    & toList
+
+            let updatedMap = Map.insert 
+                    (temperamentName temperament)
+                    updatedScales currentScales
+
+            pure updatedMap) editSubmitted
+
+    blank
+
+scaleForm :: MonadWidget t m =>
+     AppData
+  -> Temperament
+  -> Dynamic t (Temperament -> T.Text -> Validation (NonEmpty ErrorMessage) T.Text)
+  -> Scale
+  -> m (Dynamic t (Validation (NonEmpty ErrorMessage) (Temperament, Scale)))
+scaleForm appData initialTemperament isValidName initialValue = mdo
+    modalHeader "Add New Scale"
+
+    currentTemperament <- holdDyn initialTemperament =<<
+        delay 0.1 temperamentUpdated
+
+    let scaleValidation = combineDynValidations
+          [ pure $ nonEmptyText "Scale name must not be entry"
+          , isValidName <*> currentTemperament]
+
+    temperamentForm <- (validateNonNull "Temperament must be selected" <$>) <$>
+            selectTemperament appData initialTemperament
+
+    let temperamentUpdated = fmapMaybe validationToMaybe
+            (updated temperamentForm)
+
+    let periodDyn = currentTemperament <&> divisions
+
+    scaleForm <- initFormA (Scale <$>
+          formA (scaleName =. labeledEntryA "Name"
+            (validatedTextEntryDyn scaleValidation id)) <*>
+          formA (scaleIntervals =. labeledEntryA "Intervals"
+            (scaleListEntry periodDyn))) initialValue
+
+    pure $ pairForms temperamentForm scaleForm
---
+++ xen-fret/src/XenFret/App/Widgets/Fretboard.hs
@@ -0,0 +1,71 @@
+
+module XenFret.App.Widgets.Fretboard where
+
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
+import Data.Maybe
+import Graphics.Svg
+
+import XenFret
+import XenFret.Util
+import XenFret.Data
+import XenFret.AppData
+import XenFret.App.Util
+
+import Diagrams.Prelude ( renderDia, mkWidth, Diagram )
+import Diagrams.Backend.SVG
+
+data XorY = X Int | Y Int
+
+-- | Generate the formatted SVG output from a diagram.
+format :: XorY -> Diagram B -> String
+format (X x) d = T.unpack $ decodeUtf8 $ B.toStrict $
+    renderBS $ renderDia SVG (SVGOptions (mkWidth (fromIntegral x)) Nothing "" [] False) d
+format (Y y) d = T.unpack $ decodeUtf8 $ B.toStrict $
+    renderBS $ renderDia SVG (SVGOptions (mkWidth (fromIntegral y)) Nothing "" [] False) d
+
+-- | Handle the error messages from parsing the arguments.
+handleParseErrs :: Maybe Int
+    -> Maybe Int
+    -> Maybe [Int]
+    -> Maybe Int
+    -> Maybe Int
+    -> Either String (Int, Int, NonEmpty Int, XorY)
+handleParseErrs period frets tuning x y
+  -- Valid format
+  | Just period' <- period
+  , Just frets' <- frets
+  , Just tuning' <- NE.nonEmpty =<< tuning
+  , isJust x `xor` isJust y
+  = case (x,y) of
+       (Just x',_) -> Right (period',frets',tuning',X x')
+       (_,Just y') -> Right (period',frets',tuning',Y y')
+       _           -> Left "Must have either x or y specified."
+  -- Errors, invalid format
+  |  otherwise
+  = collectErrors [
+      (isNothing period, "Error parsing period, should be an integer"),
+      (isNothing frets, "Error parsing frets, should be an integer"),
+      (isNothing tuning, "Error parsing tuning, should be a list of integers, (e.a. [1,2,3])"),
+      (isNothing x && isNothing y, "Neither given an x, nor a y"),
+      (isJust x && isJust y, "Given both an x and a y, only one or the other can be given")]
+
+-- | Handle the error messages from constructing the scales and fretboard
+handleScaleFretboardErrs :: Either [String] Fretboard -> [Either [String] Scale] -> Either [String] (Fretboard,[Scale])
+handleScaleFretboardErrs fb scls = do
+    fb' <- fb
+    scls' <- sequence scls
+    pure (fb', scls')
+
+-- | Include which no. scale the errors are coming from.
+indexErrs :: [Either [String] a] -> [String]
+indexErrs xs = go 1 xs []
+    where go n [] idx = idx
+          go n ((Left errs):xs) idx = go (n+1) xs (idx++["For scale #" ++ show n ++ ": " ++ fmt errs])
+          go n ((Right _):xs)   idx = go (n+1) xs idx
+
+          fmt errs = foldl1 (\x y -> x++", "++y) errs
