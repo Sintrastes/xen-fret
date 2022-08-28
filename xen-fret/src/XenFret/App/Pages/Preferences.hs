a186f949b0dc8df1b823ceb4e811364c75dde316
Nathan Bedell <nbedell@tulane.edu>
2022-08-27 19:50:15 -0600

    Add stubs for importing and exporting data.

--- xen-fret/src/XenFret/App/Pages/Preferences.hs
+++ xen-fret/src/XenFret/App/Pages/Preferences.hs
@@ -12,3 +12,9 @@ preferencePage appDir = do
     appData <- loadAppData (appDir <> "/app_data.json")
     let currentPrefs = preferences appData
     el "p" $ text "Preferences"
+
+    clickImport <- button "Import Data"
+    
+    clickExport <- button "Export Data"
+
+    blank
