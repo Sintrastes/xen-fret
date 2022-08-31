e62b14325fc11e9376307fcac3a028e8e6603cca
Nathan Bedell <nbedell@tulane.edu>
2022-08-30 21:08:20 -0600

    Implement some basic witherable optics combinators.

--- xen-fret/src/XenFret/App/Widgets/CRUD.hs
+++ xen-fret/src/XenFret/App/Widgets/CRUD.hs
@@ -2,7 +2,16 @@
 module XenFret.App.Widgets.CRUD where
 import Reflex.Dom.Core
 import XenFret.AppData
+import Witherable(Witherable, wither)
+import Control.Applicative
 
-crud :: _ => FilePath -> (a -> m (Dynamic t (Maybe a))) -> m [a] -> ([a] -> AppData -> AppData) -> m ()
-crud appDir form getData updateData = do
+type Wither s t a b = forall f. Alternative f => (a -> f b) -> s -> f t
+type Wither' s a = Wither s s a a
+
+withered :: (Alternative f, Witherable t) => (a -> f b) -> t a -> f (t b)
+withered f = wither (optional . f)
+
+-- | Generic widget for a CRUD page that updates a collection contained in AppData.
+crud :: _ => FilePath -> (a -> m (Dynamic t (Maybe a))) -> Wither' AppData a -> m ()
+crud appDir form optic = do
     pure ()
