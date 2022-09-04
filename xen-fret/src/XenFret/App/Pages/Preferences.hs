ab2216cecd096f076adcadaf241dd1bb7f5eb3ad
Nathan Bedell <nbedell@tulane.edu>
2022-09-03 18:01:44 -0600

    Try importing JSString

--- xen-fret/src/XenFret/App/Pages/Preferences.hs
+++ xen-fret/src/XenFret/App/Pages/Preferences.hs
@@ -3,6 +3,7 @@ module XenFret.App.Pages.Preferences where
 
 import qualified Data.Text as T
 import Language.Javascript.JSaddle (liftJSM, jsg, jsg0, jsg3, jsg1, fromJSVal, fun, valToStr, JSString(..))
+import Data.JSString
 import Reflex.Dom.Core hiding(Home, button, checkbox)
 import Reflex.Dom.Extras
 import XenFret.AppData
