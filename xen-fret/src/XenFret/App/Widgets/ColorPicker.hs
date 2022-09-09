3704e6a342fc1176ce7de93eab64126b64d071c2
Nathan Bedell <nbedell@tulane.edu>
2022-09-08 20:58:36 -0600

    Add working call to bindColorPicker.

--- xen-fret/src/XenFret/App/Widgets/ColorPicker.hs
+++ xen-fret/src/XenFret/App/Widgets/ColorPicker.hs
@@ -1,12 +1,15 @@
 
 module XenFret.App.Widgets.ColorPicker where
 import Reflex.Dom.Core
-import Language.Javascript.JSaddle
+import Language.Javascript.JSaddle ( liftJSM, js, jsg2, fun, textFromJSString, valToStr, jsg, eval, js1 )
 import Data.Functor
 import Data.Text (Text)
 import Control.Monad.IO.Class
 import Control.Concurrent (threadDelay)
 import Data.Aeson.TH
+import qualified Data.Text as T
+import Control.Lens ((^.))
+import Reflex.Dom.Extras (liftFrontend, liftFrontend')
 
 data Color = Color {
     r :: Int,
@@ -14,20 +17,29 @@ data Color = Color {
     b :: Int
 }
 
+color2hex :: Color -> String
+color2hex (Color r g b) = undefined
+
+hex2color :: String -> Maybe Color
+hex2color = undefined
+
 $(deriveJSON defaultOptions ''Color)
 
 colorPicker :: (MonadWidget t m, _) => Text -> Color -> m (Dynamic t Color)
 colorPicker ident initialColor = do
-    elAttr "div" ("id" =: ("picker-" <> ident)) blank
+    postBuild <- delay 0.1 =<< getPostBuild
+    (colorUpdate, onUpdateColor) <- newTriggerEvent
 
-    el "script" $ text $ 
-        "var colorPicker = iro.ColorPicker(\"#picker-" <> ident <> "\", { layout: [{component: iro.ui.Box}, {component: iro.ui.Slider, options: {sliderType: 'hue'}}] });"
-
-    onClick <- button "test"
-
-    performEvent $ onClick <&> \_ -> do
-        currentColor <- liftJSM $ valToStr =<< eval ("colorPicker.color.hexString" :: Text)
-        liftIO $ print currentColor
-        pure ()
+    elAttr "div" ("id" =: ("picker-" <> ident)) $ do
+        performEvent $ postBuild <&> \_ ->
+            liftJSM $ do
+                colorPicker <- eval ("iro.ColorPicker(\"#picker-" <> ident <> "\", { layout: [{component: iro.ui.Box}, {component: iro.ui.Slider, options: {sliderType: 'hue'}}] });" :: T.Text)
+                liftJSM $ jsg2 ("bindColorPicker" :: T.Text) colorPicker
+                    (fun $ \_ this args -> do
+                        let firstArg = args !! 0
+                        hexString <- textFromJSString <$> valToStr firstArg
+                        -- onUpdateColor $ hex2color hexString
+                        liftIO $ putStrLn $ T.unpack hexString)
+                pure ()
 
     pure $ pure initialColor
