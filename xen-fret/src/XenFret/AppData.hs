751cc9257b1b9edd66a21c7322e75f3c3c1ec78d
Nathan Bedell <nbedell@tulane.edu>
2022-09-11 17:39:16 -0600

    Add additional scales for 16-TET.

--- xen-fret/src/XenFret/AppData.hs
+++ xen-fret/src/XenFret/AppData.hs
@@ -296,12 +296,28 @@ defaultAppData = AppData {
                   ]
             , _scales =
                   [
-                    Scale "mavila[7]"
+                    Scale "Mavilla[5]"
+                        (5 :| [2, 5, 2, 2])
+                  , Scale "Mavila[7]"
                         (2 :| [2, 2, 3, 2, 2, 3])
-                  , Scale "Lemba"
+                  , Scale "Mavilla[9]"
+                        (1 :| [2, 2, 2, 1, 2, 2, 2, 2])
+                  , Scale "Lemba[6]"
                         (3 :| [3, 2, 3, 3, 2])
-                  , Scale "Magic"
+                  , Scale "Lemba[10]"
+                        (2 :| [1, 2, 1, 2, 2, 1, 2, 1, 2])
+                  , Scale "Magic[7]"
                         (1 :| [4, 1, 4, 1, 4, 1])
+                  , Scale "Magic[10]"
+                        (1 :| [3, 1, 1, 3, 1, 1, 1, 3, 1])
+                  , Scale "Gorgo[5]"
+                        (3 :| [3, 4, 3, 3])
+                  , Scale "Gorgo[6]"
+                        (3 :| [3, 1, 3, 3, 3])
+                  , Scale "Gorgo[11]"
+                        (1 :| [2, 1, 2, 1, 2, 1, 2, 1, 2, 1])
+                  , Scale "Diminished[8]"
+                        (1 :| [3, 1, 3, 1, 3, 1, 3])
                   ]
             , _tunings =
                   [
